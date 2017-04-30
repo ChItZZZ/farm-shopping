7afe65a897295fadc6f99bf010b820f19ebaeff0
ChItZZZ <648267970@qq.com>
2017-05-01 00:20:39 +0800

    no message

--- controller/loginController.js
+++ controller/loginController.js
@@ -8,7 +8,7 @@ exports.userLogin = function (req, res, next) {
     var param = req.body;
     var username = param.username;
     var password = param.password;
-    var sql = 'SELECT password FROM t_user WHERE username = ?'
+    var sql = 'SELECT id,password FROM t_user WHERE username = ?'
     db.exec(sql,[username],function (err, result) {
         var rs = {};
         var pwd = result && result[0] && result[0].password;
