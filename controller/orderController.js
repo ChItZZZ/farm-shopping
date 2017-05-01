1a334d9453214a021e391cc8104ca2d1b7f59a67
ChItZZZ <648267970@qq.com>
2017-05-01 14:11:05 +0800

    no message

--- controller/orderController.js
+++ controller/orderController.js
@@ -38,21 +38,40 @@ exports.addOrder = function (req, res, next) {
 
 exports.findAllOrder = function (req, res, next) {
     var data = req.body;
-    var userId = data.id || 0;
+    var userId = data.user_id || 0;
     var values_order = [userId];
     var sql_order = userId != 0 ?
         'SELECT * FROM t_orderitem where user_id = ?  ':
         'SELECT * FROM t_orderitem ';
 
+    console.log(sql_order);
     db.exec(sql_order, values_order, function (err, result) {
         if (err) {
             console.log('get historyOrder from db err');
             res.end();
             return;
         }
-
-        res.json(result);
-        res.end();
+        var rs = [];
+        result.forEach(function (v, i) {
+            rs.push(v)
+            var sql_product = 'SELECT title,price FROM t_product WHERE id = ?';
+            db.exec(sql_product,[v.product_id],function (err, result) {
+                if(err) {
+                    return res.json(rs);
+                }
+                rs[i].title = result && result[0].title;
+                rs[i].price = result && result[0].price;
+                var sql_merchant = 'SELECT name FROM t_merchant WHERE mid = ?'
+                db.exec(sql_merchant,[v.mer_id],function (err, result) {
+                    if(err){
+                        return res.json(rs)
+                    }
+                    rs[i].merchantName = result && result[0].name
+                    res.json(rs);
+                    res.end();
+                })
+            })
+        })
     });
 };
 
