3e7374b0dedd99e4d61b4c688ba1498a80389879
ChItZZZ <648267970@qq.com>
2017-05-01 14:50:18 +0800

    no message

--- controller/orderController.js
+++ controller/orderController.js
@@ -52,22 +52,24 @@ exports.findAllOrder = function (req, res, next) {
         var rs = [];
         result.length && result.forEach(function (v, i) {
             rs.push(v)
-            var sql_product = 'SELECT title,price FROM t_product WHERE id = ?';
+            var sql_product = 'SELECT title,price FROM t_product p left join t_merchant m WHERE id = ? AND p.mer_id == m.mid';
             db.exec(sql_product,[v.product_id],function (err, result) {
                 if(err) {
                     return res.json(rs);
                 }
                 rs[i].title = result.length && result[0].title;
                 rs[i].price = result.length && result[0].price;
-                var sql_merchant = 'SELECT name FROM t_merchant WHERE mid = ?'
-                db.exec(sql_merchant,[v.mer_id],function (err, result) {
-                    if(err){
-                        return res.json(rs)
-                    }
-                    rs[i].merchantName = result.length && result[0].name
-                    res.json(rs);
-                    res.end();
-                })
+                rs[i].imgUrl = result.length && result[0].pic_url;
+                rs[i].merchantName = result.length && result[0].name
+                // var sql_merchant = 'SELECT name FROM t_merchant WHERE mid = ?'
+                // db.exec(sql_merchant,[v.mer_id],function (err, result) {
+                //     if(err){
+                //         return res.json(rs)
+                //     }
+                //     rs[i].merchantName = result.length && result[0].name
+                //     res.json(rs);
+                //     res.end();
+                // })
             })
         })
     });
