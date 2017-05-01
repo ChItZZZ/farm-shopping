3817d9f940f607e49d55c1867184e63c5e00b76b
ChItZZZ <648267970@qq.com>
2017-05-01 15:20:07 +0800

    no message

--- controller/orderController.js
+++ controller/orderController.js
@@ -35,45 +35,48 @@ exports.addOrder = function (req, res, next) {
         res.end()
     })
 }
+function order(rs) {
 
+
+}
 exports.findAllOrder = function (req, res, next) {
     var data = req.body;
     var userId = data.user_id || 0;
     var values_order = [userId];
     var sql_order = userId != 0 ?
-        'SELECT * FROM t_orderitem where user_id = ?  ':
-        'SELECT * FROM t_orderitem ';
+        'SELECT o.*,p.price,p.title,p.imgUrl FROM t_orderitem o left join t_product p on o.product_id = p.id where user_id = ?  ':
+        'SELECT o.*,p.price,p.title,p.imgUrl FROM t_orderitem o left join t_product p on o.product_id = p.id';
 
     db.exec(sql_order, values_order, function (err, result) {
         if (err) {
             console.log('get historyOrder from db err');
             return res.end();
         }
-        var rs = [];
-        result.length && result.forEach(function (v, i) {
-            rs.push(v)
-            var sql_product = 'SELECT * FROM t_product left join t_merchant ON t_product.mer_id = t_merchant.mid WHERE t_product.id = ?';
-            // var sql_product = 'SELECT * FROM t_product  WHERE t_product.id = ?';
-            db.exec(sql_product,[v.product_id],function (err, result) {
-                if(err) {
-                    return res.json(rs);
-                }
-                rs[i].title = result.length && result[0].title;
-                rs[i].price = result.length && result[0].price;
-                rs[i].imgUrl = result.length && result[0].pic_url;
-                rs[i].merchantName = result.length && result[0].name
-                res.json(rs)
-                // var sql_merchant = 'SELECT name FROM t_merchant WHERE mid = ?'
-                // db.exec(sql_merchant,[v.mer_id],function (err, result) {
-                //     if(err){
-                //         return res.json(rs)
-                //     }
-                //     rs[i].merchantName = result.length && result[0].name
-                //     res.json(rs);
-                //     res.end();
-                // })
-            })
-        })
+        res.json(result)
+        // result.length && result.forEach(function (v, i) {
+        //     rs.push(v)
+        //     var sql_product = 'SELECT * FROM t_product left join t_merchant ON t_product.mer_id = t_merchant.mid WHERE t_product.id = ?';
+        //     // var sql_product = 'SELECT * FROM t_product  WHERE t_product.id = ?';
+        //     db.exec(sql_product,[v.product_id],function (err, result) {
+        //         if(err) {
+        //             return res.json(rs);
+        //         }
+        //         rs[i].title = result.length && result[0].title;
+        //         rs[i].price = result.length && result[0].price;
+        //         rs[i].imgUrl = result.length && result[0].pic_url;
+        //         rs[i].merchantName = result.length && result[0].name
+        //         res.json(rs)
+        //         // var sql_merchant = 'SELECT name FROM t_merchant WHERE mid = ?'
+        //         // db.exec(sql_merchant,[v.mer_id],function (err, result) {
+        //         //     if(err){
+        //         //         return res.json(rs)
+        //         //     }
+        //         //     rs[i].merchantName = result.length && resulvt[0].name
+        //         //     res.json(rs);
+        //         //     res.end();
+        //         // })
+        //     })
+        // })
     });
 };
 
